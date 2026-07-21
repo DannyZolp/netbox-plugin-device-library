a65a59094ad4b9cc62177e13be8b29c63744a5aa
Danny Zolp <12878237+DannyZolp@users.noreply.github.com>
2026-07-21 08:36:37 -0500

    setup plugin structure

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from netbox-plugin-device-library!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ plugin/__init__.py
@@ -0,0 +1,21 @@
+__author__ = "Danny Zolp"
+__email__ = "danny@zolp.io"
+__version__ = "0.1.0"
+
+
+from netbox.plugins import PluginConfig
+
+
+class DeviceLibraryConfig(PluginConfig):
+    name = "netbox_plugin_device_library"
+    verbose_name = "Device Library"
+    description = "A simple plugin that integrates the Netbox device library with your instance to allow for simple importing."
+    author= "Danny Zolp"
+    author_email = "danny@zolp.io"
+    version = __version__
+    base_url = "plugin"
+    min_version = "4.5.0"
+    max_version = "4.5.99"
+
+
+config = DeviceLibraryConfig
